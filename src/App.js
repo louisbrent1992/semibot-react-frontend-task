137dac7b8df68a420ccf244c4c2bb01de36033df
Louis Brent <louisbrent1992@gmail.com>
2023-02-24 17:01:25 -0800

    updated App component to render site content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
+import styled from "styled-components";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import SearchBar from "./components/SearchBar";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 3rem;
+`;
 
 function App() {
   return (
-    <div >
-      <header >
-        
+    <Container>
+      <header>
+        <NavBar />
       </header>
-    </div>
+      <section>
+        <SearchBar />
+      </section>
+      <section>
+        {/* Filter panel to allow user to filter items by one or more criteria */}
+      </section>
+      <Footer />
+    </Container>
   );
 }
 
